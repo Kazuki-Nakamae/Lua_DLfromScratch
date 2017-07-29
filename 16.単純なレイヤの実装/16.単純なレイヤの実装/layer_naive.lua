c884137dbd6204e7069b05a40fc2aeb4021349e6
Nakamae_ Kazuki <kazuki_nakamae@hotmail.com>
2017-07-29 11:32:59 +0900

    update

---
+++ 16.単純なレイヤの実装/16.単純なレイヤの実装/layer_naive.lua
@@ -0,0 +1,71 @@
+--Copyright (C) 2017  Kazuki Nakamae
+--Released under MIT License
+--license available in LICENSE file
+
+--- MulLayerクラス（オブジェクト）
+-- 乗算レイヤを実装
+MulLayer={}
+MulLayer.new = function()
+    local obj = {}
+
+    --メンバ変数
+    obj.x = nil
+    obj.y = nil
+
+    -- @function self.forward()
+    -- 順伝搬
+    -- @param x 入力１{Type:number}
+    -- @param x 入力２{Type:number}
+    -- @return 乗算結果{Type:number}
+    obj.forward = function(self, x, y)
+        self.x = x
+        self.y = y
+        local out = x * y
+
+        return out
+    end
+
+    -- @function self.backward()
+    -- 逆伝搬
+    -- @param dout 層の出力に対する微分{Type:number}
+    -- @return 層の各入力に対する微分{Type:number}
+    obj.backward = function(self, dout)
+        local dx = dout * self.y
+        local dy = dout * self.x
+
+        return dx, dy
+    end
+
+    return obj
+end
+
+--- AddLayerクラス（オブジェクト）
+-- 加算レイヤを実装
+AddLayer={}
+AddLayer.new = function()
+    local obj = {}
+
+    -- @function self.forward()
+    -- 順伝搬
+    -- @param x 入力１{Type:number}
+    -- @param x 入力２{Type:number}
+    -- @return 加算結果{Type:number}
+    obj.forward = function(self, x, y)
+        local out = x + y
+
+        return out
+    end
+
+    -- @function self.backward()
+    -- 逆伝搬
+    -- @param dout 層の出力に対する微分{Type:number}
+    -- @return 層の各入力に対する微分{Type:number}
+    obj.backward = function(self, dout)
+        local dx = dout * 1
+        local dy = dout * 1
+
+        return dx, dy
+    end
+
+    return obj
+end
